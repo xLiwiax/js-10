0a75460a937d032493ada22687b3489c77fef0c4
xLiwiax <boliwia333@gmail.com>
2022-02-23 10:23:13 +0100

    zadanie 10

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let num1 = 2;
 let num2 = 5;
-function fun1(num1, num2){
-    if (num1>num2){
-        return num1;
+function fun1(a, b){
+    if (a > b){
+        return a;
     } else{
-        return num2;
+        return b;
     }
 }
-console.log(fun1);
+console.log(fun1(num1, num2));
